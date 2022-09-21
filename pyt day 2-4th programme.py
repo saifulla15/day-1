7224b88e9c5d9c583c087a5937b6b6ffaee09d87
saifulla15 <104437948+saifulla15@users.noreply.github.com>
2022-09-21 10:02:22 +0530

    Add files via upload

---
+++ pyt day 2-4th programme.py
@@ -0,0 +1,25 @@
+list1-[]
+
+list2=[]
+
+11-int(input("enter the length1:")) 12-int(input("enter the length2:"))
+
+for i in range(0,11): e-int(input()
+
+listi.append(e)
+
+print(list1)
+
+for i in range(0,12):
+
+ele-int(input())
+
+list2.append(ele)
+
+print(list2)
+
+print("the list 1 is:"estr(list1)) print("the list 2 is: "+str(list2))
+
+res-sorted(list1+list2)
+
+print("the coombined sorted list is: "+str(res))|
