509dedcc43a1bd74599aeaf65fbfe8bfd7a8eb5a
saifulla15 <104437948+saifulla15@users.noreply.github.com>
2022-09-21 14:46:46 +0530

    DAY3-program 8

---
+++ 28.anagram.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+# initializing list
+test_list = ["eat","tea","tan","ate","hat","bat"]
+
+# printing original list
+print("The original list : " + str(test_list))
+
+# using defaultdict() + sorted() + values()
+# Grouping Anagrams
+temp = defaultdict(list)
+for ele in test_list:
+    temp[str(sorted(ele))].append(ele)
+res = list(temp.values())
+
+# print result
+print("The grouped Anagrams : " + str(res))
