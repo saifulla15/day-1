0029d29fd4ab172bd9d81c706e2335f502585fb4
saifulla15 <104437948+saifulla15@users.noreply.github.com>
2022-09-24 16:39:21 +0530

    Add files via upload

---
+++ 48.py
@@ -0,0 +1,27 @@
+def shuffle(l1,l2):
+  c=[]
+  if len(l1)!=0 and len(l2)!=0:
+    for i in range(min(len(l1), len(l2))):
+      c.extend([l1[i],l2[i]])      
+    c.extend(l1[i+1:] or l2[i+1:])
+  else:
+    c.extend(l1[0:] or l2[0:])      
+  return (c)
+
+l = []
+s2 = []
+while True:
+  s = input()
+  if s == '*':
+    break
+  else:
+    l.append(int(s))
+
+while True:
+  s = input()
+  if s == '*':
+    break
+  else:
+    s2.append(int(s))
+
+print(shuffle(l,s2))
