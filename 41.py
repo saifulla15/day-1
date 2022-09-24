2bb37385589a617e91fd9c076c3bb6c5abd2d8b5
saifulla15 <104437948+saifulla15@users.noreply.github.com>
2022-09-24 16:33:21 +0530

    DAY5-program1

---
+++ 41.py
@@ -0,0 +1,29 @@
+def lengthOfLastWord(a):
+
+    l = 0
+
+
+
+    x = a.strip()
+
+
+    for i in range(len(x)):
+
+        if x[i] == " ":
+
+            l = 0
+
+        else:
+
+            l += 1
+
+    return l
+
+
+if __name__ == "__main__":
+
+    inp = "Hello World"
+
+    print("The length of last word is", 
+
+                 lengthOfLastWord(inp))
